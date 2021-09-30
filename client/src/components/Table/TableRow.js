f2f4753c45f69b1d3176eac8519353c96c283eb8
Griffin Sharp <gsharpdev@gmail.com>
2021-09-29 23:45:06 -0700

    Add url (#6)

--- client/src/components/Table/TableRow.js
+++ client/src/components/Table/TableRow.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { dataIsUrl } from '../../utils/tableUtils';
+
+import colorStyles from '../../styles/colors.module.css';
+
+// TODO: Edit this to be more generic / format multiple types (not just URL).
+const UrlView = (url) => (
+  <td>
+    <a
+      className={colorStyles.purple}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={url}
+      >
+      link
+    </a>
+  </td>
+);
+
+
 const TableRow = props => (
   <tbody>
     <tr>
-      { props.row.map((el, index) => <td key={index}>{el}</td>) }
+      {
+        props.row.map((el, index) => dataIsUrl(el, index) ? UrlView(el) : <td key={index}>{el}</td>)
+      }
     </tr>
   </tbody>
 );
--- client/src/utils/tableUtils.js
+++ client/src/utils/tableUtils.js
@@ -1,14 +1,15 @@
 export const HEADERS = ['SALE TYPE', 'SOLD DATE', 'TYPE', 'ADDRESS', 'CITY', 'STATE', 'ZIP', 'PRICE', 'BD', 'BA', 'LOC', 'SQFT', 'LOT SIZE', 'YEAR BUILT', 'DAYS ON MARKET', '$/SQFT', 'HOA/MONTH', 'STATUS', 'OH START', 'OH END', 'URL', 'SOURCE', 'MLS#', 'FAV', 'INTERESTED', 'LAT', 'LONG'];
 export const SIMPLE_HEADERS = ['SALE TYPE', 'TYPE', 'ADDRESS', 'CITY', 'STATE', 'ZIP', 'PRICE', 'BD', 'BA', 'LOC', 'SQFT', 'YEAR BUILT', 'DAYS ON MARKET', '$/SQFT', 'STATUS'];
 
+export const getLink = (row) => {
+  const urlIndex = HEADERS.indexOf('URL');
+  return row[urlIndex];
+}
+
 // Given an index, should the data be included in the simplfied table view.
 export const indexIsSimple = (index) => {
   const headerText = HEADERS[index];
   return SIMPLE_HEADERS.includes(headerText);
 }
 
-export const getLink = (row) => {
-  const urlIndex = HEADERS.indexOf('URL');
-  return row[urlIndex];
-}
-
+export const dataIsUrl = (data, index) => HEADERS[index] === 'URL' && data.includes('redfin.com');
